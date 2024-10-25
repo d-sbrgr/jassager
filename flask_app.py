43b72cd7f1340e2a7182864eba06fe7cacfad91d
David Schurtenberger <schurtenberger.david@gmail.com>
2024-10-25 14:40:28 +0200

    chore: add gcloud support

---
+++ flask_app.py
@@ -0,0 +1,31 @@
+import os
+import logging
+
+from jass.service.player_service_app import PlayerServiceApp
+from bots import FullMCTS
+
+
+def create_app():
+    """
+    This is the factory method for flask. It is automatically detected when flask is run, but we must tell flask
+    what python file to use:
+
+        export FLASK_APP=player_service.py
+        export FLASK_ENV=development
+        flask run --host=0.0.0.0 --port=8888
+    """
+    logging.basicConfig(level=logging.DEBUG)
+
+    # create and configure the app
+    app = PlayerServiceApp('player_service')
+
+    # add some players
+    app.add_player('jassager', FullMCTS())
+
+    return app
+
+
+if __name__ == '__main__':
+   app = create_app()
+   app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+
